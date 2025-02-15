0ecb83e079bb46ce81696fd51a003e074ee58e5d
0326ashi <ashikaridi2002@gmail.com>
2025-02-15 16:37:23 +0530

    Created components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RegistrationpageComponent } from './registrationpage/registrationpage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "dashboard", component: DashboardComponent},
+    {path: "registration", component: RegistrationpageComponent},
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+  })
+  
+  export class AppRoutingModule { }
